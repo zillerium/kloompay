3831027d5c8bca1a4c8b5dd264d5b00620136860
lizeth9797 <karina.ortiz@fintecheando.mx>
2021-03-06 21:20:37 -0600

    login as website with Trevor's code

--- KloomPay/src/app/login/login.page.ts
+++ KloomPay/src/app/login/login.page.ts
@@ -8,9 +8,9 @@ import {Router} from '@angular/router'
 })
 export class LoginPage implements OnInit {
 
-  email:string;
+ /*  email:string;
   passwoword: string;
-
+ */
   constructor(public router: Router) { }
 
   ngOnInit() {
@@ -22,6 +22,37 @@ export class LoginPage implements OnInit {
       this.router.navigate(['/tabs/tab1']);
     }).catch(err=> alert('Incorrect Data')); */
   }
+ 
+  processFailedLogin() {
+  }
+
+  kloomlogin(){
+   var userName = (<HTMLInputElement>document.getElementById('email')).value;
+   var userPassword = (<HTMLInputElement>document.getElementById('password')).value;
+ 
+   var xhttp = new XMLHttpRequest();
+   xhttp.open("POST",  "https://kloompay.com:3000/api/checkUser", true);
+   xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+   xhttp.send(JSON.stringify({ "userName": userName, "userPassword": userPassword }));
+   xhttp.onreadystatechange = function(){
+   if (this.readyState == 4 && this.status == 200) {
+       var data = JSON.parse(this.response);
+       if (data.response === "ok") {
+
+        if (data.db.result) {
+          window.location.href = "/tabs/tab1"
+        } else {
+        }       
+       } else {
+        document.getElementById("message").innerHTML="There was an error";
+      }
+     } 
+   };
+  }
+
+    
+   
+
 
 
 }
