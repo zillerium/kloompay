23b411570f9913dfd04c8461985a945cf9166411
Glaisher <glaisher.wiki@gmail.com>
2021-03-09 11:07:01 +0500

    Add dashboard

--- kloom/kloom.js
+++ kloom/kloom.js
@@ -151,4 +151,54 @@ function kloomlogin() {
   }
   
   function processFailedLogin() {}
-  
+
+function getCookie(name) {
+  var value = `; ${document.cookie}`;
+  var parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function getUrlQueryParam(key) {
+  var url = new URL(window.location.href);
+  return url.searchParams.get(key);
+}
+
+
+function checkIsLoggedIn (cb) {
+  var sessionid = getCookie('session');
+  var userName = getUrlQueryParam('userName');
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST",  "https://kloompay.com:3000/api/checkSessionId", true);
+  xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify({ "userName": userName, "sessionid": sessionid }));
+  xhttp.onreadystatechange = function(){
+  if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.response);
+      console.log(data);
+      if (data.response === "ok" && data.valid === true) {
+          cb();
+      } else {
+          document.getElementById("message").innerHTML="There was an error";
+    }
+    }
+  };
+}
+
+
+function getPayments (cb) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST",  "https://kloompay.com:3000/api/getPayments", true);
+  xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  xhttp.send();
+  xhttp.onreadystatechange = function(){
+  if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.response);
+      if (data.response === "ok") {
+          cb(data);
+      } else {
+          document.getElementById("message").innerHTML="There was an error";
+    }
+    }
+  };
+}
